ee6906c5ce056e37b8e4d991d58bc467231be17a
MANOJ_SM <manumedleri@gmail.com>
2025-02-12 20:40:40 +0530

    added styles

--- vite-project/src/components/Book.jsx
+++ vite-project/src/components/Book.jsx
@@ -5,7 +5,7 @@ function Book(props){
         <>
             <div className="book-card">
                 <img src={props.Bookdetails.coverImageUrl} alt="" height="250px" width="200px" className="book-cover" />
-                <div>
+                <div className="book-details">
                     <h2 className="book-title" >{props.Bookdetails.title}</h2>
                     <p className="book-author" >{props.Bookdetails.author}</p>
                     <p className="book-description" >{props.Bookdetails.description}</p>
